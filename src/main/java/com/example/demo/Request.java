bd5eb353832967c69e90d1662ab0fc50c59e0779
thegodofwar9732 <muhtasimmc@gmail.com>
2020-12-10 20:31:50 -0500

    added remaining fields for request class

--- src/main/java/com/example/demo/Request.java
+++ src/main/java/com/example/demo/Request.java
@@ -4,6 +4,10 @@ import java.time.LocalDateTime;
 
 public class Request {
 	private String body;
+	private String status;
+	private String detail;
+	private final LocalDateTime createdTimeStamp = LocalDateTime.now();
+	private LocalDateTime modifiedTimeStamp = LocalDateTime.now();
 
 	public String getBody() {
 		return body;
